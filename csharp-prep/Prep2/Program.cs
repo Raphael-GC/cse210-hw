5b8e58542abc1ad450899a814d895aaeb4e52de4
Raphael-GC <raphaelcarneirodev@gmail.com>
2025-01-13 17:07:57 -0300

    ✨ Feat: Done the Exercise2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,37 @@
 using System;
 
 class Program
-{
+{  
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Please insert your grade (%): ");
+        string percentage = Console.ReadLine();
+        int grade = int.Parse(percentage);
+
+        string letter;
+        string sign = "";
+
+        if (grade >= 90)
+            letter = "A";
+        else if (grade >= 80)
+            letter = "B";
+        else if (grade >= 70)
+            letter = "C";
+        else if (grade >= 60)
+            letter = "D";
+        else
+            letter = "F";
+
+        int remainder = grade % 10;
+        if (remainder >= 7 && letter != "A" && letter != "F")
+            sign = "+";
+        else if (remainder <= 3 && letter != "F")
+            sign = "-";
+
+        Console.WriteLine($"Your letter is {letter}{sign}");
+        if (grade >= 70)
+            Console.WriteLine("Congratulations!! You pass the class!");
+        else
+            Console.WriteLine("Sorry, but you have to try hard next year!");
     }
 }
