86b6e2c16b9b92f64ca6aa5cbc6dc0cd08eb642f
Raphael-GC <raphaelcarneirodev@gmail.com>
2025-01-14 15:25:47 -0300

    ✨ Feat: Done the Exercise3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -2,8 +2,41 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random roulette = new Random();
+
+        int randomNumber = roulette.Next(1, 101);
+
+        int guess = 0;
+        int attempts = 0;
+
+        while (guess != randomNumber)
+        {
+            Console.Write("What is your guess? ");
+            
+            if (int.TryParse(Console.ReadLine(), out guess))
+            {
+                attempts++;
+
+                if (guess < randomNumber)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (guess > randomNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else
+                {
+                    Console.WriteLine($"You guessed it in {attempts} attempts.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Please, type a valid number.");
+            }
+        }
     }
 }
+
